2d7b6b72c45bc2556c30ad4c374a8b62d30c5fae
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-02 22:00:53 +0530

    commit

--- avenue_real_estate.py
+++ avenue_real_estate.py
@@ -16,3 +16,6 @@ def split_train_test(data,test_ratio):
     test_indices=shuffled[:test_set_size]
     train_indices=shuffled[test_set_size:]
     return data.iloc[train_indices],data.iloc[test_indices]
+
+train_set,test_set=split_train_test(housing,0.2)
+print(f"Rows in train set:{len(train_set)}\nRows in test set:{len(test_set)}\n")
