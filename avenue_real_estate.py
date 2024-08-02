1ed88df076dcf9e862732a188a46b2e1bc47ade7
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-02 20:20:35 +0530

    save

--- avenue_real_estate.py
+++ avenue_real_estate.py
@@ -2,3 +2,4 @@ import pandas as pd
 url = 'https://raw.githubusercontent.com/Vrushal7/Real-Estate-price-prediction-through-machine-learning/main/data.csv'
 housing = pd.read_csv(url)
 print(housing.head())
+print(housing.info())
