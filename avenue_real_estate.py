d66b05810d88f2c62b95d35adad4cb4e278b0302
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-02 21:50:59 +0530

    commit

--- avenue_real_estate.py
+++ avenue_real_estate.py
@@ -9,6 +9,10 @@ import matplotlib.pyplot as plt
 housing.hist(bins=50,figsize=(20,15))
 
 #Train test splitting
+import numpy as np
 def split_train_test(data,test_ratio):
     shuffled=np.random.permutation(len(data))
     test_set_size=int(len(data)*test_ratio)
+    test_indices=shuffled[:test_set_size]
+    train_indices=shuffled[test_set_size:]
+    return data.iloc[train_indices],data.iloc[test_indices]
