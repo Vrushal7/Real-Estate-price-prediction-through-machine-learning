e97a4aec918fdb4479a932d51e6aec268f8395c8
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-03 14:29:21 +0530

    commit

--- avenue_real_estate.py
+++ avenue_real_estate.py
@@ -93,3 +93,8 @@ mse=mean_squared_error(housing_labels,housing_predictions)
 rmse=np.sqrt(mse)
 print(mse)
 print(rmse)
+
+#Using cross validation
+from sklearn.model_selection import cross_val_score
+scores=cross_val_score(model,housing_num_tr,housing_labels,scores="neg_mean_squared_error",cv=10)
+rmse_scores=np.sqrt(-scores)
