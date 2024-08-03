79bea91e8fa67d2b4b8167b8f1a99ac2b8bf3130
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-03 11:06:34 +0530

    commit

--- avenue_real_estate.py
+++ avenue_real_estate.py
@@ -28,3 +28,7 @@ def split_train_test(data,test_ratio):
 from sklearn.model_selection import train_test_split
 train_set,test_set=train_test_split(housing,test_size=0.2,random_state=42)
 print(f"Rows in train set:{len(train_set)}\nRows in test set:{len(test_set)}\n")
+
+from sklearn.model_selection import StratifiedShuffleSplit
+split=StratifiedShuffleSplit(housing,housing['chas'])
+for train_index,test_index in split.split()
