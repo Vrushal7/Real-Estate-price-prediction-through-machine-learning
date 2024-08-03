258cde5da19471a54eefce0bbc3f166cf9c52e4a
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-03 11:54:14 +0530

    save

--- avenue_real_estate.py
+++ avenue_real_estate.py
@@ -50,5 +50,3 @@ housing.plot(kind="scatter",x="rm",y="medv",alpha=0.8)
 housing["taxrm"]=housing["tax"]/housing["rm"]
 print(housing.head())
 housing.plot(kind="scatter",x="taxrm",y="medv",alpha=0.8)
-
-#Missing Attribute
