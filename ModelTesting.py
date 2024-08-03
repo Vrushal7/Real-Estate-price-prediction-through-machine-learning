f16164287ec6f7c26d79e53d474ca408aa04f92c
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-03 15:50:59 +0530

    commit

--- ModelTesting.py
+++ ModelTesting.py
@@ -1,7 +1,15 @@
-from joblib import dump, load
-import  numpy as np
-model=load('AvenueRealEstates.joblib')
-input=np.array([[-0.43942006  3.12628155 -1.12165014 -0.27288841 -1.42262747 -0.23979304
- -1.31238772  2.61111401 -1.0016859  -0.5778192  -0.97491834  0.41164221
- -0.86091034]])
-model.predict(input)
+from joblib import load
+import numpy as np
+
+# Load the model
+model = load('AvenueRealEstates.joblib')
+
+# Correct the array syntax
+input = np.array([[-0.43942006, 3.12628155, -1.12165014, -0.27288841, -1.42262747, -0.23979304,
+                   -1.31238772, 2.61111401, -1.0016859, -0.5778192, -0.97491834, 0.41164221,
+                   -0.86091034]])
+
+# Make predictions
+predictions = model.predict(input)
+print(predictions)
+
