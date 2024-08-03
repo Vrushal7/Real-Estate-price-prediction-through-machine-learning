0ab36b3b27d794ed69218c8cb2b9b6e3dbb51a0f
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-08-03 13:50:56 +0530

    save

--- avenue_real_estate.py
+++ avenue_real_estate.py
@@ -63,5 +63,6 @@ housing_tr.describe()
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 my_pipeline=Pipeline([
-    ('imputer',SimpleImputer(strategy="median"))
+    ('imputer',SimpleImputer(strategy="median")),
+    ('std_scaler',StandardScaler()),
 ])
